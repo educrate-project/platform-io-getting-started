651d818d19787a45479f4c12d98c28d75b50810e
kjuulh <hermansendev@gmail.com>
2021-02-07 13:09:09 +0100

    Add initial platform IO

---
+++ PlatformIO Getting Started/src/main.cpp
@@ -0,0 +1,29 @@
+// src/main.cpp
+
+#include <Arduino.h>
+
+const int chosenLEDPin = LED_BUILTIN;
+const int delayBetweenBlinksMs = 1000;
+
+void turnLEDOn(bool turnOn) {
+  if (turnOn) {
+    digitalWrite(chosenLEDPin, HIGH);
+  } else {
+  digitalWrite(chosenLEDPin, LOW);
+  }
+}
+
+void sleepOneSecond() {
+  delay(delayBetweenBlinksMs);
+}
+
+void setup() {
+  pinMode(chosenLEDPin, OUTPUT);
+}
+
+void loop() {
+  turnLEDOn(true);
+  sleepOneSecond();
+  turnLEDOn(false);
+  sleepOneSecond();
+}
